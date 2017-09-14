333c598483c6562479a87af1a77a18d2eb721486
lloydst <lloyd.stumpel@gmail.com>
2017-09-14 12:21:15 +0200

    file cleanup

--- js/index.js
+++ js/index.js
@@ -12,27 +12,26 @@ app.controller('bomberControl', function($scope) {
     
     $( document ).ready(function() {
         console.log( "ready!" );
+        start();
         
-        console.log(xplayer,yplayer);
 
-        // makes playerlocation visable
-        if ($('cell:contains('+xplayer+'.'+yplayer+')')) {
-            console.log(xplayer + '.' + yplayer + ' i am player location');
-            $('cell:contains('+xplayer+'.'+yplayer+')').removeClass("path").addClass('player');
-        } 
+        
+        
     });
-    function start() {
-        xplayer = 5;
-        yplayer = 5;
-        console.log(xplayer,yplayer)
-    }
-    function playerlocation(params) {
-        if ($('cell:not(:contains('+xplayer+'.'+yplayer+'))')) {
-            $('cell:not(:contains('+xplayer+'.'+yplayer+'))').removeClass("player")
-            console.log(xplayer + '.' + yplayer + ' i am the new player location')
-            $('cell:contains('+xplayer+'.'+yplayer+')').addClass("player");
+    // makes playerlocation visable
+    function playerlocation() {
+        $('cell:not(:contains('+xplayer+'.'+yplayer+'))').removeClass("player"); //clears it so only 1 player should be there (doesnt ully workf)
+        
+        $('cell:contains('+xplayer+'.'+yplayer+')').addClass("player");
+        $('cell:contains('+xplayer+'.'+yplayer+')').removeClass("path");
         }
-    }
+        function start() {
+            xplayer = 5;
+            yplayer = 5;
+            playerlocation();
+            
+        }
+    
     
     
     function left() {
@@ -86,6 +85,10 @@ app.controller('bomberControl', function($scope) {
             playerlocation()
         }
         
+    }
+
+    function bomb(){
+        $('cell').hasClass('player')
     }
         //   function   key keyid   type:
         // ''   up      38  up      ArrowKey
